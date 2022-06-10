0e2968a1a8e50093049985d0ed960738c80c10c0
VoNgocThanhHao <19004058@st.vlute.edu.vn>
2022-06-11 00:19:22 +0700

    ..

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => "Võ Ngọc Thanh Hào",
+            'email' => "haovnt187@gmail.com",
+            'password' => bcrypt("123456"),
+            'permission' => 3,
+        ]);
+
+        DB::table('profiles')->insert([
+            'image' => "public/mySource/imgs/avatars/unknow.jpg",
+            'email' => "haovnt187@gmail.com",
+            'user_id' => 1,
+        ]);
+    }
+}
