6a42db6613cb83253e0b662e84f326a95463cbe0
VoNgocThanhHao <19004058@st.vlute.edu.vn>
2022-06-12 23:14:31 +0700

    .

--- resources/views/admin/transaction/transaction_home.blade.php
+++ resources/views/admin/transaction/transaction_home.blade.php
@@ -126,12 +126,6 @@
                     }else{
                         $('#stt_bill').val(0)
                     }
-                }else{
-                    Swal.fire(
-                        'Thành công!',
-                        result.message,
-                        'success'
-                    )
                 }
 
                 $('.btnUpdate').attr('data',data.id)
